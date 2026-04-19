33623217a2b371dc3365f87ed27cde938ff878f0
daikadbdaikabd-design <daikadbdaikabd@gmail.com>
2026-04-19 22:01:58 +0700

    index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function startBot() {
   console.log("Đang khởi động bot...")
 
   bot = mineflayer.createBot({
-    host: "191.96.231.27",
-    port: 10570,
+    host: "darkblademc.joinmc.world",
+    port: 20674,
     username: "MeMayBeo",
     version: "1.20.1"
   })
